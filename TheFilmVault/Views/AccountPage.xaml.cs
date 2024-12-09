d303d1ae4c9514fbc16549bb41e2c4b6a5885746
Austin-Riddle50 <w10181026@usm.edu>
2024-12-09 13:52:06 -0600

    Added clicked event functionality to the movies, watchlist, and home page. in .cs file.

--- TheFilmVault/Views/AccountPage.xaml.cs
+++ TheFilmVault/Views/AccountPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class AccountPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnMoviesButtonClicked(object sneder, EventArgs e)
+	{
+		App.Current.MainPage = new MovieView();
+	}
+
+	private void OnHomeButtonClicked(object sender, EventArgs e)
+	{
+		App.Current.MainPage = new AppStartPage();
+	}
+
+	private void OnWatchlistButtonClicked(object sender, EventArgs e)
+	{
+		App.Current.MainPage = new Watchlist();
+	}
 }
